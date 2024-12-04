cf6580c6516000e0cbdec579f8448717c61ce9ac
muhammedminhajkk <muhammedminhajkk1@gmail.com>
2024-12-04 18:02:26 +0530

    debug banner removed

--- lib/features/home_page/services/images_services.dart
+++ lib/features/home_page/services/images_services.dart
@@ -7,7 +7,7 @@ import 'package:http/http.dart' as http;
 class ImageApi {
   static Future<PhotoResponseModel> getTrendingImages() async {
     final url =
-        Uri.parse("https://api.pexels.com/v1/popular/?page=1&per_page=10");
+        Uri.parse("https://api.pexels.com/v1/popular/?page=1&per_page=50");
     const headers = {
       "Authorization":
           "MJ6BQu2hYcewqFabFsMGgXHhX8QjREVEcoz7VDDYEcQYNQPxacXmfoix"
